f8a857f1c981ce819997742ce666a1f6b5acb604
aroum <aroum24@gmail.com>
2024-07-06 14:51:46 +0300

    LED init

--- config/boards/shields/gbEnki/status_led.c
+++ config/boards/shields/gbEnki/status_led.c
@@ -23,12 +23,25 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/settings/settings.h>
+#include <zmk/events/split_peripheral_status_changed.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-#define BAT_LED_NODE_1 DT_NODELABEL(bat_led_1)
-#define BAT_LED_NODE_2 DT_NODELABEL(bat_led_2)
-#define BAT_LED_NODE_3 DT_NODELABEL(bat_led_3)
-#define STATUS_LED_NODE DT_NODELABEL(status_led)
+#ifdef invert_led_order
+    #if IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+        #define BAT_LED_NODE_1 DT_NODELABEL(bat_led_3)
+        #define BAT_LED_NODE_2 DT_NODELABEL(bat_led_2)
+        #define BAT_LED_NODE_3 DT_NODELABEL(bat_led_1)
+    #else
+        #define BAT_LED_NODE_1 DT_NODELABEL(bat_led_1)
+        #define BAT_LED_NODE_2 DT_NODELABEL(bat_led_2)
+        #define BAT_LED_NODE_3 DT_NODELABEL(bat_led_3)
+    #endif
+#else
+    #define BAT_LED_NODE_1 DT_NODELABEL(bat_led_1)
+    #define BAT_LED_NODE_2 DT_NODELABEL(bat_led_2)
+    #define BAT_LED_NODE_3 DT_NODELABEL(bat_led_3)
+#endif
+
 
 #define LED_BLINK_PROFILE 200
 #define LED_BLINK_CONN 140
@@ -42,6 +55,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 // #define real_bat_animation
 #define show_bat_status_all_time
 #define show_led_idle
+#define invert_led_order
 
 struct led
 {
@@ -50,19 +64,12 @@ struct led
     unsigned int gpio_flags;
 };
 
-struct led status_led = {
-    .gpio_dev = DEVICE_DT_GET(DT_GPIO_CTLR(STATUS_LED_NODE, gpios)),
-    .gpio_pin = DT_GPIO_PIN(STATUS_LED_NODE, gpios),
-    .gpio_flags = GPIO_OUTPUT | DT_GPIO_FLAGS(STATUS_LED_NODE, gpios),
-};
-
 enum
 {
     BAT_1,
     BAT_2,
     BAT_3
 };
-
 struct led battery_leds[] = {
     [BAT_1] = {
         .gpio_dev = DEVICE_DT_GET(DT_GPIO_CTLR(BAT_LED_NODE_1, gpios)),
@@ -93,11 +100,14 @@ static inline void ledOFF(const struct led *led)
 
 static void led_all_OFF()
 {
-    ledOFF(&status_led);
+
+
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
     for (int i = 0; i < (sizeof(battery_leds) / sizeof(struct led)); i++)
     {
         ledOFF(&battery_leds[i]);
     }
+#endif
 }
 
 void led_configure(const struct led *led)
@@ -108,7 +118,7 @@ void led_configure(const struct led *led)
         printk("Error %d: failed to configure pin %d\n", ret, led->gpio_pin);
         return;
     }
-    ledOFF(led);
+    led_all_OFF();
 }
 
 void blink(const struct led *led, uint32_t sleep_ms, const int count)
@@ -158,7 +168,7 @@ void led_bat_animation()
 
     switch (led_i)
     {
-    case 1:
+        case 1:
         if (level > 70)
         {
             ledON(&battery_leds[0]);
@@ -201,16 +211,17 @@ void led_bat_animation()
         led_i++;
         break;
     }
+    // led_all_OFF();
+    // return;
 #else
-        for (int i = 0; i <= 3; i++)
-        {
-            ledON(&battery_leds[i]);
-            k_msleep(LED_BATTERY_BLINK);
-        }
-        led_all_OFF();
-        return;
+    for (int i = 0; i < 2; i++)
+    {
+        ledON(&battery_leds[i]);
+        k_msleep(LED_BATTERY_BLINK);
+    }
+    led_all_OFF();
+    return;
 #endif
-    // k_timer_start(&bat_timer, K_SECONDS(LED_BATTERY_SLEEP_SHOW / 1000), K_NO_WAIT);
 }
 
 void led_bat_handler(struct k_work *work)
@@ -232,13 +243,19 @@ K_TIMER_DEFINE(bat_timer, led_bat_timer_handler, NULL);
 
 // Checking the connection status
 struct k_timer led_timer;
-bool led_conn_check_working = false;
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+    bool led_conn_check_working = false;
+#else
+    bool peripheral_ble_connected = false;
+#endif
 
 void check_ble_connection()
 {
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
     if (zmk_ble_active_profile_is_connected())
     {
         led_conn_check_working = false;
+        led_all_OFF();
     }
     else
     {
@@ -247,12 +264,32 @@ void check_ble_connection()
         {
             return;
         }
-
-        blink_once(&status_led, LED_BLINK_CONN);
+        for (int i = 0; i < (sizeof(battery_leds) / sizeof(struct led)); i++)
+        {
+            blink_once(&battery_leds[i], LED_BLINK_CONN);
+            k_msleep(LED_BATTERY_BLINK/2);
+        }   
+        led_all_OFF();
         led_conn_check_working = true;
         // Restart timer for next status check
         k_timer_start(&led_timer, K_SECONDS(4), K_NO_WAIT);
     }
+#else
+    if (peripheral_ble_connected)
+    {
+        return;
+    }
+    else
+    {
+        for (int i = 0; i < (sizeof(battery_leds) / sizeof(struct led)); i++)
+            {
+                blink_once(&battery_leds[i], LED_BLINK_CONN);
+                k_msleep(LED_BATTERY_BLINK/2);
+            }   
+        led_all_OFF();
+        k_timer_start(&led_timer, K_SECONDS(4), K_NO_WAIT);
+    }
+#endif
 }
 
 void led_check_connection_handler(struct k_work *work)
@@ -327,20 +364,17 @@ void bat_show_once_work_handler(struct k_work *work)
         {
             blink(&battery_leds[0], LED_BATTERY_BLINK, 3);
         }
-
-        led_all_OFF();
         check_ble_connection();
+        led_all_OFF();
     }
     else
     {
-        // NOTE: Basically timer will go on and on until we get level different that zero.
+        // NOTE: Basically timer will go on and on until we get level different than zero.
     }
 }
 
 static int led_init(const struct device *dev)
 {
-    led_configure(&status_led);
-
     for (int i = 0; i < (sizeof(battery_leds) / sizeof(struct led)); i++)
     {
         led_configure(&battery_leds[i]);
@@ -351,42 +385,59 @@ static int led_init(const struct device *dev)
 
 SYS_INIT(led_init, APPLICATION, 32);
 
-// Show leds on profile changing
-int led_profile_listener(const zmk_event_t *eh)
-{
-    const struct zmk_ble_active_profile_changed *profile_ev = NULL;
-    if ((profile_ev = as_zmk_ble_active_profile_changed(eh)) == NULL)
+#if IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+    // Show leds on profile changing
+    int led_profile_listener(const zmk_event_t *eh)
     {
+        const struct zmk_ble_active_profile_changed *profile_ev = NULL;
+        if ((profile_ev = as_zmk_ble_active_profile_changed(eh)) == NULL)
+        {
+            return ZMK_EV_EVENT_BUBBLE;
+        }
+        // For profiles 1-3 blink appropriate leds.
+        if (profile_ev->index <= 2)
+        {
+            ledON(&battery_leds[profile_ev->index]);
+            k_msleep(LED_BLINK_PROFILE);
+            led_all_OFF();
+        }
+
+        if (!led_conn_check_working)
+        {
+            check_ble_connection();
+        }
+
         return ZMK_EV_EVENT_BUBBLE;
     }
-    // For profiles 1-3 blink appropriate leds.
-    if (profile_ev->index <= 2)
+
+    ZMK_LISTENER(led_profile_status, led_profile_listener)
+    ZMK_SUBSCRIPTION(led_profile_status, zmk_ble_active_profile_changed);
+#else
+    int led_profile_listener(const zmk_event_t *eh)
     {
-        for (int i = 0; i <= profile_ev->index; i++)
-        {
-            ledON(&battery_leds[i]);
+        const struct zmk_split_peripheral_status_changed *status = as_zmk_split_peripheral_status_changed(eh);
+        
+        peripheral_ble_connected = status->connected;
+        if (!peripheral_ble_connected) {
+            check_ble_connection();
         }
-        k_msleep(LED_BLINK_PROFILE);
-        led_all_OFF();
+
+        return ZMK_EV_EVENT_BUBBLE;
     }
 
-    if (!led_conn_check_working)
-    {
-        check_ble_connection();
-    }
-
-    return ZMK_EV_EVENT_BUBBLE;
-}
-
-ZMK_LISTENER(led_profile_status, led_profile_listener)
-ZMK_SUBSCRIPTION(led_profile_status, zmk_ble_active_profile_changed);
+    ZMK_LISTENER(led_profile_status, led_profile_listener)
+    ZMK_SUBSCRIPTION(led_profile_status, zmk_split_peripheral_status_changed);
+#endif
 
 // Restore activity after return to active state
 int led_state_listener(const zmk_event_t *eh)
 {
     enum zmk_activity_state state = zmk_activity_get_state();
-
-    if (state == ZMK_ACTIVITY_ACTIVE && !led_conn_check_working)
+    #if IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
+        if (state == ZMK_ACTIVITY_ACTIVE && !led_conn_check_working)
+    #else
+        if (state == ZMK_ACTIVITY_ACTIVE)
+    #endif
     {
         check_ble_connection();
     }
@@ -419,11 +470,10 @@ int led_usb_conn_listener(const zmk_event_t *eh)
 
     // if (usb_ev->conn_state == ZMK_USB_CONN_POWERED || usb_ev->conn_state == ZMK_USB_CONN_HID)
     if (usb_ev->conn_state == ZMK_USB_CONN_POWERED)
-
     {
         led_bat_animation();
     }
-
+    led_all_OFF();
     return ZMK_EV_EVENT_BUBBLE;
 }
 
@@ -437,5 +487,24 @@ void show_battery()
 
 void hide_battery()
 {
-    led_all_OFF();
+    // led_all_OFF();
 }
+// # zmk-gbEnki
+
+// XXX 100%
+// XXB 80%
+// XX  60%
+// XB  40%
+// X   20%
+// B   LOW
+// --------
+// XOO bt1
+// OXO bt2
+// OOX bt3
+// --------
+// XOO>XXO>XXX USB
+// XOO>OXO>OOX no connect
+
+// O - off
+// X - on
+// B - blink
